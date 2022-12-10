8c8db7c68700259ba54f6432737f518ba1f7faef
Santa <avotina@protonmail.com>
2022-12-10 17:51:25 +0200

    basic strings and methods

--- index.py
+++ index.py
@@ -1,3 +1,19 @@
-msg='My name is Kate'
-new_msg=msg[11:]+msg[7:10]+' '+msg[0:2]+msg[2:7]
-print(new_msg.upper())
+# msg='My name is Kate'
+# new_msg=msg[11:]+msg[7:10]+' '+msg[0:2]+msg[2:7]
+# print(new_msg.upper())
+
+# msg="""Hello, this is my
+# first multiline string,
+# yes it is!"""
+# print(msg)
+
+# msg='Welcome to Python 101: Strings'
+# print(msg.replace('Python', 'Java'))
+# print('Python' in msg)
+
+name='MARTA'
+color = 'RED'
+msg = '[' + name + '] loves the color ' + color.lower() + '!'
+msg1 = f'[{name.capitalize()}] loves the color {color.lower()}!'
+print(msg)
+print(msg1)
